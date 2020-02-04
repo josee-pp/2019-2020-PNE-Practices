95489a28fb5f1c4a698396c201e9ba31a8dc7995
joseepp <joseepinzonp@gmail.com>
2020-02-04 15:51:00 +0100

    just another test...

--- Session-02/sumN.py
+++ Session-02/sumN.py
@@ -1,6 +1,13 @@
 # -- 1 + 2 + 3 + ... + 20
 # -- 1 + ... + 100
 
+def sumn(n):
+    res = 0
+    for i in range(1, n):
+        res += i
+    return res
+
+userinput = int(input("Enter number n: "))
 res20 = 0
 
 for i in range(1,21):
@@ -11,6 +18,6 @@ res100 = 0
 for i in range(1, 101):
     res100 += i
 
-print("sum of 1-20: ", res20)
-print("sum of 1-100: ", res100)
+print("sum of 1-", userinputm, "is: ", sumn(userinput))
+
 
