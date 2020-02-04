c2e5dc921d5f5c33f3e0819c3836f8e04899a202
joseepp <joseepinzonp@gmail.com>
2020-02-04 16:26:21 +0100

    just another test...

--- Session-02/E2.py
+++ Session-02/E2.py
@@ -1,15 +1,15 @@
 # S2 E2
 
 def fibon(n):
-    if n < 0:
+    if n+1 < 0:
         print("Incorrect input")
-    elif n == 1:
+    elif n+1 == 1:
         return 0
-    elif n == 2:
+    elif n+1 == 2:
         return 1
     else:
         return fibon(n-1) + fibon(n-2)
 
-print(fibon(5))
-print(fibon(10))
-print(fibon(15))
+print(fibon(6))
+print(fibon(11))
+print(fibon(16))
