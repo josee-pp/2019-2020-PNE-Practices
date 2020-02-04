1b0849dcd3c1a71b214f289ed0097dd136741586
joseepp <joseepinzonp@gmail.com>
2020-02-04 16:18:49 +0100

    just another test...

--- Session-02/fibonacci.py
+++ Session-02/fibonacci.py
@@ -1,16 +1,10 @@
 
-n=int(input("Enter the terms"))
 a=0
 b=1
 
-if n<=0:
-    print("The requested series is",f)
-else:
-    print(a,b,end=" ")
-    for x in range(2,n):
-        next=a+b
-        print(next,end=" ")
-        a=b
-        b=next
+for i in range(11):
+    a = b
+    b = a + b
+    print(a, end=" ")
 
 
