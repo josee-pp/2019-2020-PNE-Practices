26d4e4e9f72d25c6ba051b45fcce24dde88072b4
joseepp <joseepinzonp@gmail.com>
2020-02-11 16:12:28 +0100

    S4 E4

---
+++ Session-04/body.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+filename = "U5.txt"
+file_contents = Path(filename).read_text()
+
+bodystr = " "
+
+lines = file_contents.split('\n')
+body = lines[1:]
+
+bodystr = bodystr.join(body)
+
