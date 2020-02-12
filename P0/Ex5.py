ffd4354550ff507fabab24c1d0dfb4443d853d2f
joseepp <joseepinzonp@gmail.com>
2020-02-12 18:34:07 +0100

    s5

--- P0/Ex5.py
+++ P0/Ex5.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 FOLDER = "/home/alumnos/joseepp/PycharmProjects/2019-2020-PNE-Practices/Session-04/"
 FILENAME = ["U5", "ADA", "FRAT1", "FXN", "U5"]
-BASES = ["A", "C", "T", "G"]
+BASES = ["A", "T", "C", "G"]
 
 for element in FILENAME:
     dnafile = FOLDER + element + ".txt"
+    BASESLIST = list(seq_count(dnafile))
+    print("Gene", element, ":", dict(zip(BASES, BASESLIST)))
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -32,21 +32,28 @@ def seq_count_base(seq, base):
     return (count)
 
 def seq_count(seq):
+    file_contents = Path(seq).read_text()
+    seq = ""
+    lines = file_contents.split('\n')
+    body = lines[1:]
+    seq = seq.join(body).replace(" ", "")
     countA = 0
+    countT = 0
     countC = 0
     countG = 0
-    countT = 0
     for i in seq:
         if i == "A":
             countA = countA + 1
+        elif i == "T":
+            countT = countT + 1
         elif i == "C":
             countC = countC + 1
         elif i == "G":
             countG = countG + 1
-        elif i == "T":
-            countT = countT + 1
         else:
             next
-    return countA, countC, countG, countT
+    return countA, countT, countC, countG
+
+
 
 
