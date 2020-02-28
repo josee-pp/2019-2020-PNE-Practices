c3f1e3974ce3b8f41fcbdc9212aefc3a17436352
joseepp <joseepinzonp@gmail.com>
2020-02-28 14:48:17 +0100

    s9

--- P2/Ex6.py
+++ P2/Ex6.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from Client0 import Client
+from Seq1 import Seq
 
 def seq_divider(filename):
     bodystr = ""
@@ -38,11 +39,18 @@ PORT = 8080
 
 c = Client(IP, PORT)
 
+msg = "Sending FRAT1 Gene to the server, in fragments of 10 bases..."
+
+response = c.debug_talk(msg)
+print(c)
+
 fragments = seq_divider(filename)[0]
 dnabody = seq_divider(filename)[1]
 
+snull = Seq()
+
 print(f"Gene FRAT1: {dnabody}")
 for a in fragments:
-    print(f"To Server: {a}")
-    response = c.talk(a)
+    print(f"Fragment {fragments.index(a) + 1}: {a}")
+    response = c.debug_talk(f"Fragment {fragments.index(a) + 1}: {a}")
 
