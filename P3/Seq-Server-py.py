1b33c1fa0d9fa543caf22c239c4294a2bd25f62a
joseepp <joseepinzonp@gmail.com>
2020-03-04 17:59:53 +0100

    s11

--- P3/Seq-Server-py.py
+++ P3/Seq-Server-py.py
@@ -12,6 +12,25 @@ def GET(n):
     for seq in seqlist:
         return (f"{seqlist[n]}\n")
 
+def INFO(seq):
+    s = Seq(seq)
+    seqlen = s.len()
+    counti = 0
+    baseslist = ["A", "C", "G", "T"]
+    countlist = []
+    percentlist = []
+    for i in seq:
+        if i in baseslist:
+            counti = counti + 1
+        countlist.append(counti)
+    for count in countlist:
+        percentage = round((count/seqlen)*100, 2)
+        percentlist.append(percentage)
+    return (seq, seqlen, countlist, percentlist)
+
+
+
+
 
 # Configure the Server's IP and PORT
 PORT = 8080
@@ -78,5 +97,9 @@ while True:
             cs.close()
 
         elif msg.split(" ")[0] == "INFO":
+            baseslist = ["A", "C", "G", "T"]
             seq = msg.split(" ")[1]
-
+            seqlen = INFO(seq)[1]
+            countlist = INFO(seq)[2]
+            percentlist = INFO(seq)[2]
+            print(INFO(seq))
