0490aa388ea0933ef93e7fb3cff58b855f383545
José <joseepinzonp@gmail.com>
2020-03-18 14:30:03 +0100

    finished

---
+++ P4/EX6.py
@@ -0,0 +1,108 @@
+import socket
+import termcolor
+import pathlib
+
+# -- Server network parameters
+IP = "192.168.1.48"
+PORT = 8080
+
+def read_fasta(file):
+    # -- Open and read the html files
+    file_contents = pathlib.Path(file).read_text().split("\n")[1:]
+    body = "".join(file_contents)
+    return body
+
+def process_client(s):
+    # -- Receive the request message
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    # -- Split the request messages into lines
+    lines = req.split('\n')
+
+    # -- The request line is the first
+    req_line = lines[0]
+
+    print("Request line: ", end="")
+    print(req_line)
+
+    # -- Generate the response message
+    # It has the following lines
+    # Status line
+    # header
+    # blank line
+    # Body (content to send)
+
+    # This new contents are written in HTML language
+    folder = r"C:\Users\José\PycharmProjects\2019-2020-PNE-Practices\P4"
+    file_req = req_line.split()[1]
+    # file_req is similar to req_line (GET / HTTP/1.1), only with Path /info/(Base).
+
+    if file_req == "/":
+        file = "\index.html"
+
+    elif "/info/A" in file_req:
+        file = "\A.html"
+
+    elif "/info/C" in file_req:
+        file = "\C.html"
+
+    elif "/info/G" in file_req:
+        file = "\G.html"
+
+    elif "/info/T" in file_req:
+        file = "\T.html"
+
+    else:
+        file = "\Error.html"
+
+    body = read_fasta(folder + file)
+
+    # -- Status line: We respond that everything is ok (200 code)
+    status_line = "HTTP/1.1 200 OK\n"
+
+    # -- Add the Content-Type header
+    header = "Content-Type: text/html\n"
+
+    # -- Add the Content-Length
+    header += f"Content-Length: {len(body)}\n"
+
+    # -- Build the message by joining together all the parts
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+# -------------- MAIN PROGRAM
+# ------ Configure the server
+# -- Listening socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Setup up the socket's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Become a listening socket
+ls.listen()
+
+print("SEQ Server configured!")
+
+# --- MAIN LOOP
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server Stopped!")
+        ls.close()
+        exit()
+    else:
+
+        # Service the client
+        process_client(cs)
+
+        # -- Close the socket
+        cs.close()
