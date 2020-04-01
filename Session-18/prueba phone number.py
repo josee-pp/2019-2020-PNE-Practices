0cf24343d2bcf276c79f533f742ac6c8249e74a5
José <joseepinzonp@gmail.com>
2020-04-01 15:22:12 +0200

    finished

--- Session-18/prueba phone number.py
+++
@@ -1,4 +0,0 @@
-"phoneNumber": [[{"number": "1111", "type": "home"},{"number": "333", "type" : "office"}], {"number": "9999-564", "type": "home"}, [{"number": "1111", "type": "home"},
-      {"number": "333", "type" : "office"},
-        {"number": "1111", "type": "home"}]
-      ]
