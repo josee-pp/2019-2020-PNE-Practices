a85724888a3be5a78bb3051e95c36bb1b6061d96
José <joseepinzonp@gmail.com>
2020-04-03 13:58:38 +0200

    changes

---
+++ P7/EX02-gene-id.py
@@ -0,0 +1,33 @@
+import http.client
+
+SERVER = "rest.ensembl.org"
+ENDPOINT = "/sequence/id"
+PARAMS = "/ENSG00000157764?content-type=text/plain"
+URL = SERVER + ENDPOINT + PARAMS
+
+print()
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", "/sequence/id/ENSG00000157764?content-type=text/plain")
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# -- Print the data
+print(data1)
