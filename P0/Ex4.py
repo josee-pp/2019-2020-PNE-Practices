7d7b5aefd565d8b7255a6ac93265336712613dd5
josee-pp <joseepinzonp@gmail.com>
2020-04-21 20:45:33 +0200

    Ex4

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -5,8 +5,12 @@ FOLDER = "/home/alumnos/joseepp/PycharmProjects/2019-2020-PNE-Practices/Session-
 FILENAME = ["U5", "ADA", "FRAT1", "FXN", "U5"]
 BASES = ["A", "C", "T", "G"]
 
+# - - For each gene (element of FILENAME list) we print its name and we obtain the
+# entire filename (dnafile):
+
 for element in FILENAME:
     dnafile = FOLDER + element + ".txt"
     print("Gene: " + element)
+    # For each base we count it using seq_count_base:
     for i in BASES:
         print(i + ":", seq_count_base(dnafile, i))
