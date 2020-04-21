5abbbdb12976fdbf672d77aeac0c52ce9c41d374
josee-pp <joseepinzonp@gmail.com>
2020-04-21 20:42:15 +0200

    Ex3

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -4,6 +4,9 @@ from pathlib import Path
 FOLDER = "/home/alumnos/joseepp/PycharmProjects/2019-2020-PNE-Practices/Session-04/"
 FILENAME = ["U5", "ADA", "FRAT1", "FXN", "U5"]
 
+# We print the length for each sequence of the list FILENAME:
+
 for i in FILENAME:
     dnafile = FOLDER + i + ".txt"
+    # - - The seq_len function Calculate the total number of bases in the sequence:
     print("Gene", i, "---> Length: ", seq_len(dnafile))
