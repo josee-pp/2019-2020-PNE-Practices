b5b515d09b7186e21dafc5bfcb3dabfa41523c61
josee-pp <joseepinzonp@gmail.com>
2020-04-22 14:05:47 +0200

    Finished

--- Session-08/client1.py
+++ Session-08/client1.py
@@ -1,10 +1,11 @@
 import socket
 
+# -- Parameters of the server to talk to
 IP = "192.168.1.48"
 PORT = 8080
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IP, PORT))
-s.send(str.encode("Se viene goteoremixxxxxxxxxxxxxxx"))
+s.send(str.encode("Message"))
 msg = s.recv(2000)
 print("Message from the server: ", msg.decode("utf-8"))
--- Session-10/client-test.py
+++ Session-10/client-test.py
@@ -1,13 +1,15 @@
 from Client0 import Client
 from termcolor import colored
 
-
+# -- Parameters of the server to talk to
 IP = "127.0.0.1"
 PORT = 8080
 
+# -- Create the client object
 c = Client(IP, PORT)
 count = 0
 
+# -- Each message is counted, and both message and response are printed. The program stops at the 5th message.
 while True:
     msg = f"Message {count}"
     msgtoserver = colored(msg, "blue")
