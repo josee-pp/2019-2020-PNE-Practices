36b3f0f8d7e545a2cb3bb02c380420d9f39c90fb
josee-pp <joseepinzonp@gmail.com>
2020-04-28 12:52:14 +0200

    not finished

--- Final-Project/client.py
+++ Final-Project/client.py
@@ -2,16 +2,18 @@ import http.client
 from termcolor import colored
 from Seq1 import Seq
 
-ID = gene_dict[gene]
 SERVER = "rest.ensembl.org"
-ENDPOINT = "/sequence/id"
-PARAMS = f"/{ID}?content-type=text/x-fasta;type=genomic"
-URL = SERVER + ENDPOINT + PARAMS
-
-print()
-print(f"Server: {SERVER}")
-print(f"URL: {URL}")
 
 # Connect with the server
 conn = http.client.HTTPConnection(SERVER)
 
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+print(data1)
